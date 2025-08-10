92aa0930c8ea167c2a28466687b4bcf0c83a4f1d
kalmante <ryanjayleyva@proton.me>
2025-08-10 08:17:30 -0400

    feat(nvim): add vim-tmux-navigator editor plugin

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -20,6 +20,22 @@ M.spec = {
       vim.g.matchup_enable = 1
     end,
   },
+
+  -- vim-tmux-navigator
+  {
+    'christoomey/vim-tmux-navigator',
+
+    keys = {
+      { '<C-h>', desc = 'Navigate to left tmux pane' },
+      { '<C-j>', desc = 'Navigate to down tmux pane' },
+      { '<C-k>', desc = 'Navigate to up tmux pane' },
+      { '<C-l>', desc = 'Navigate to right tmux pane' },
+    },
+
+    config = function()
+      vim.g.tmux_navigator_no_mappings = 1
+    end,
+  },
 }
 
 return M.spec
