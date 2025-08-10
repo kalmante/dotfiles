c0085fe5f380aaf0f8f0a93e0fd352d0d8cc7d87
kalmante <ryanjayleyva@proton.me>
2025-08-10 06:41:28 -0400

    feat(nvim): add basic key mappings for insert exit and clearing search highlights
    - Map 'jk' in insert mode to exit to normal mode
    - Add <leader>nh to clear search highlights in normal mode
    - Set leader key to space

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local keymap = vim.keymap
+
+vim.g.leader = ' '
+
+-- Exit insert mode with jk
+keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode with jk' })
+
+-- Clear search highlights
+keymap.set('n', '<leader>nh', ':nohl<CR>', { desc = 'Clear search highlights' })
