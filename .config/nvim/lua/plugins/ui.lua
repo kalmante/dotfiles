0be5ef56ba5643aed7d4416979244db32c11eb48
kalmante <ryanjayleyva@proton.me>
2025-08-10 07:44:25 -0400

    chore(nvim): disable snacks.nvim ui indent and scroll features

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -12,6 +12,15 @@ M.spec = {
     'nvim-lualine/lualine.nvim',
     enabled = false,
   },
+
+  -- Disable snacks indent scroll
+  {
+    'snacks.nvim',
+    opts = {
+      indent = { enabled = false },
+      scroll = { enabled = false },
+    },
+  },
 }
 
 return M.spec
