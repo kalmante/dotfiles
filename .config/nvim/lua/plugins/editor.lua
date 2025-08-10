b12544bbc77298c8368dd64a74136e67d3771eba
kalmante <ryanjayleyva@proton.me>
2025-08-10 08:16:05 -0400

    feat(nvim): add vim-matchup editor plugin

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -6,6 +6,20 @@ M.spec = {
     'folke/flash.nvim',
     enabled = false,
   },
+
+  -- vim-matchup
+  {
+    'andymass/vim-matchup',
+    event = { 'BufReadPost', 'BufNewFile' },
+    init = function()
+      vim.g.matchup_matchparen_offscreen = { method = 'popup' }
+      vim.g.matchup_matchparen_deferred = 1
+      vim.g.matchup_matchparen_timeout = 200
+      vim.g.matchup_matchparen_insert_timeout = 100
+      vim.g.matchup_override_vimtex = 1
+      vim.g.matchup_enable = 1
+    end,
+  },
 }
 
 return M.spec
