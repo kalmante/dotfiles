f585e9bcd2b26333cbc6e07f5f9543f84ac3209e
kalmante <ryanjayleyva@proton.me>
2025-08-10 07:06:27 -0400

    refactor(nvim): remove mapleader, maplocalleader config and simplify keymap usage

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,10 @@
-local keymap = vim.keymap
-
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
-
 -- Exit insert mode with jk
-keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode with jk' })
+vim.keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode with jk' })
 
 -- Clear search highlights
-keymap.set('n', '<leader>nh', ':nohl<CR>', { desc = 'Clear search highlights' })
+vim.keymap.set(
+  'n',
+  '<leader>nh',
+  ':nohl<CR>',
+  { desc = 'Clear search highlights' }
+)
