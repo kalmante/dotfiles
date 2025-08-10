aa5c4cf20223b512b9956b7078fb266b64621d3f
kalmante <ryanjayleyva@proton.me>
2025-08-10 08:27:58 -0400

    feat(nvim): add incline.nvim ui plugin

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -13,7 +13,7 @@ M.spec = {
     enabled = false,
   },
 
-  -- Disable snacks.nvim indent, scroll and dashboard
+  -- Disable snacks indent, scroll and dashboard
   {
     'snacks.nvim',
     opts = {
@@ -21,6 +21,44 @@ M.spec = {
       scroll = { enabled = false },
       dashboard = { enabled = false },
     },
+  },
+
+  -- incline
+  {
+    'b0o/incline.nvim',
+    event = 'BufReadPre',
+    priority = 1200,
+    dependencies = { 'craftzdog/solarized-osaka.nvim' },
+
+    config = function()
+      local colors = require('solarized-osaka.colors').setup()
+      require('incline').setup {
+        highlight = {
+          groups = {
+            InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
+            InclineNormalNC = {
+              guifg = colors.violet500,
+              guibg = colors.base03,
+            },
+          },
+        },
+        window = { margin = { vertical = 0, horizontal = 1 } },
+        hide = {
+          cursorline = true,
+        },
+        render = function(props)
+          local filename =
+            vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+          if vim.bo[props.buf].modified then
+            filename = '[+]' .. filename
+          end
+
+          local icon, color =
+            require('nvim-web-devicons').get_icon_color(filename)
+          return { { icon, guifg = color }, { ' ' }, { filename } }
+        end,
+      }
+    end,
   },
 }
 
