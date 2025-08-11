db993fc80fdde66517db0e4e9e93ec1c9aacbb8b
kalmante <ryanjayleyva@proton.me>
2025-08-11 07:36:27 -0400

    feat(nvim): add incline plugin

---
+++ .config/nvim/lua/kalmante/plugins/incline.lua
@@ -0,0 +1,39 @@
+-- https://github.com/craftzdog/dotfiles-public/blob/master/.config/nvim/lua/plugins/ui.lua
+
+local M = {}
+
+M.spec = {
+  'b0o/incline.nvim',
+  event = 'BufReadPre',
+  priority = 1200,
+  dependencies = { 'craftzdog/solarized-osaka.nvim' },
+
+  config = function()
+    local colors = require('solarized-osaka.colors').setup()
+    require('incline').setup {
+      highlight = {
+        groups = {
+          InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
+          InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+        },
+      },
+      window = { margin = { vertical = 0, horizontal = 1 } },
+      hide = {
+        cursorline = true,
+      },
+      render = function(props)
+        local filename =
+            vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+        if vim.bo[props.buf].modified then
+          filename = '[+]' .. filename
+        end
+
+        local icon, color =
+            require('nvim-web-devicons').get_icon_color(filename)
+        return { { icon, guifg = color }, { ' ' }, { filename } }
+      end,
+    }
+  end,
+}
+
+return M.spec
