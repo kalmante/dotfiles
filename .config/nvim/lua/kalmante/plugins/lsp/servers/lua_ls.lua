c94778b39d7ba0abc242dea658dda3164756cf08
kalmante <ryanjayleyva@proton.me>
2025-08-11 02:46:14 -0400

    feat(nvim): add type annotations and root_dir to lua_ls config to fix workspace path issues

--- .config/nvim/lua/kalmante/plugins/lsp/servers/lua_ls.lua
+++ .config/nvim/lua/kalmante/plugins/lsp/servers/lua_ls.lua
@@ -1,5 +1,10 @@
+local util = require 'lspconfig.util'
+
 local M = {}
 
+--- Setup the Lua LSP server with given capabilities.
+--- @param server table LSP server instance from lspconfig
+--- @param capabilities table Capabilities object passed to the LSP server
 function M.setup(server, capabilities)
   if #vim.lsp.get_clients({ name = 'lua_ls' }) > 0 then
     return
@@ -7,6 +12,10 @@ function M.setup(server, capabilities)
 
   server.setup {
     capabilities = capabilities,
+    root_dir = util.root_pattern('.git', 'init.lua', '.stowrc', 'wezterm.lua')(
+    ---@diagnostic disable-next-line: undefined-field
+      vim.loop.fs_realpath(vim.fn.expand('%:p'))
+    ) or vim.fn.getcwd(),
     settings = {
       Lua = {
         runtime = { version = 'LuaJIT' },
