168ef338e0730f8385e83a28a60b2c0304935b74
kalmante <ryanjayleyva@proton.me>
2025-08-10 23:57:43 -0400

    refactor(nvim): remove format_after_save and clean up unified tools config

--- .config/nvim/lua/kalmante/plugins/conform.lua
+++ .config/nvim/lua/kalmante/plugins/conform.lua
@@ -8,14 +8,10 @@ M.spec = {
   opts = {
     format_on_save = {
       lsp_format = 'fallback',
-      timeout_ms = 1000,
-    },
-    format_after_save = {
-      lsp_format = 'fallback',
+      timeout_ms = 1500,
     },
     formatters_by_ft = {
-      ['*'] = { 'codespell' },
-      ['_'] = { 'trim_whitespace' },
+      ['_'] = { 'codespell', 'trim_whitespace' },
       astro = { 'prettier' },
       css = { 'prettier' },
       go = { 'goimports', 'gofmt' },
@@ -47,12 +43,16 @@ M.spec = {
             'eslint.config.mjs',
             'eslint.config.ts',
             'package.json',
-          }, { upward = true, path = ctx.filename })[1] ~= nil
+          }, {
+            upward = true,
+            path = ctx.filename
+          })[1] ~= nil
         end,
       },
     },
 
-    -- Set log level to avoid noisy output but still capture formatter errors
+    -- Set log level to avoid noisy output
+    -- but still capture formatter errors
     log_level = vim.log.levels.ERROR,
     notify_no_formatters = true,
     notify_on_error = true,
@@ -62,9 +62,21 @@ M.spec = {
     local conform = require 'conform'
     conform.setup(opts)
 
-    vim.keymap.set({ 'n', 'v' }, '<leader>cf', function()
-      conform.format { async = false, lsp_format = 'fallback' }
-    end, { desc = 'Format file or range (Conform)' })
+    vim.keymap.set({
+        'n',
+        'v'
+      },
+      '<leader>cf',
+      function()
+        conform.format {
+          async = false,
+          quiet = false,
+          lsp_format = 'fallback'
+        }
+      end,
+      {
+        desc = 'Format file or range (Conform)'
+      })
   end,
 }
 
